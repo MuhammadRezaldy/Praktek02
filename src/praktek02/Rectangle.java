b28c87aa86b9c95390b2196ede3248e2a6fd61c0
MuhammadRezaldy <rezaldymm1@gmail.com>
2022-12-15 14:28:26 +0800

    Membuat attribut dan behavior

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package praktek02;
 
-/**
- *
- * @author win 10
- */
 public class Rectangle {
+     double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("==================");
+        System.out.println("Panjang :"+panjang);
+        System.out.println("Lebar :"+lebar);
+        System.out.println("==================");
+    }
 }
